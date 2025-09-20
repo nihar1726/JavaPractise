0a0dd72506967fe0f92a45213025283290f0df4d
nihar1726 <pnihar2705@gmail.com>
2025-09-20 19:45:41 +0530

    More arithmetic operators in Java

---
+++ CurrencyConversion.java
@@ -0,0 +1,19 @@
+public class CurrencyConversion {
+    public static void main(String[] args) {
+        double usdAmount = 100.0; // Amount in USD
+        double conversionRateUsdEur = 0.85; // Example conversion rate from USD to EUR
+
+        // Convert USD to EUR
+        double eurAmount = usdAmount * conversionRateUsdEur;
+        System.out.println(usdAmount + " USD is equal to " + eurAmount + " EUR");
+
+        // Convert EUR back to USD
+        double convertedBackToUsd = eurAmount / conversionRateUsdEur;
+        System.out.println(eurAmount + " EUR is equal to " + convertedBackToUsd + " USD");
+
+        // Now, let's Convert USD to INR
+        double conversionRateUsdInr = 88.10; // Example conversion rate from USD to INR
+        double inrAmount = usdAmount * conversionRateUsdInr;
+        System.out.println(usdAmount + " USD is equal to " + inrAmount + " INR");
+    }
+}
